a32c53a5bdda9aff71e1c585e05196ac6008ba2f
w-cook <66490610+w-cook@users.noreply.github.com>
2026-07-27 09:39:38 -0400

    Add reliable reorder message processing

---
+++ InventoryReorderPlatform.Processor/Processing/IReorderMessageProcessor.cs
@@ -0,0 +1,13 @@
+﻿using InventoryReorderPlatform.Contracts.Messages;
+
+namespace InventoryReorderPlatform.Processor.Processing;
+
+public interface IReorderMessageProcessor
+{
+    Task<ReorderProcessingResult> ProcessAsync(
+        ReorderRequestedMessage message,
+        string messageId,
+        string? rawPayload,
+        int deliveryCount,
+        CancellationToken cancellationToken = default);
+}
---
+++ InventoryReorderPlatform.Processor/Processing/ReorderProcessingOutcome.cs
@@ -0,0 +1,8 @@
+﻿namespace InventoryReorderPlatform.Processor.Processing;
+
+public enum ReorderProcessingOutcome
+{
+    Processed,
+    DuplicateSkipped,
+    Failed
+}
